7144374fe40e6b31036153be7356afcc0d653e65
Kelin Field <kfield111@hotmail.com>
2020-08-09 03:32:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,6 +44,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |holiday, supplies|
   winter_supplies = supplies.values.flatten
   end
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
