f092f5271a0a4f33f3a8e9eb693cd74e37d41247
Kelin Field <kfield111@hotmail.com>
2020-08-09 04:42:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holidays|
+  holiday_hash.each do |season|
     if  holidays.values.include? ("BBQ")
       puts "test"
     end
