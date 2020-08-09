74d8193e284c5145a7bd9265d77d5dca523ec75f
Kelin Field <kfield111@hotmail.com>
2020-08-09 03:55:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-winter_supplies = []
   supplies_christmas = holiday_hash[:winter][:christmas]
   supplies_christmas.map
 
