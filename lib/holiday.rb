74001d686883984f6b491a9fa161cf477f640748
Kelin Field <kfield111@hotmail.com>
2020-08-09 03:35:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,8 +41,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].each do |holiday, supplies|
-  winter_supplies = supplies.values.flatten
+  holiday_hash[:winter].each do |holiday|
+    winter_supplies = holiday.values.flatten
   end
   winter_supplies
 end
