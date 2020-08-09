821c61a5707d6cf9430051901c7a89720cf85222
Kelin Field <kfield111@hotmail.com>
2020-08-09 02:52:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   new_holiday = holiday_hash[:season]
-  new_fall_holiday[:holiday_name]
+  new_holiday[:holiday_name]
 
   holiday_hash
 binding.pry
