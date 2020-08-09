9789577b1fd256159952b0d752023c3b94883993
Kelin Field <kfield111@hotmail.com>
2020-08-09 04:16:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday, supplies|
+    puts "#{season.capitalize}: #{holiday}:"
+      holiday.each
 
 end
 
