326f043347d9f95472bdad9550eec3177ddbbe5f
Kelin Field <kfield111@hotmail.com>
2020-08-09 04:56:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+new_array = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       supplies.each do |supply|
